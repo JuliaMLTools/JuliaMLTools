2cde147c3a4154613d361f226da0ed8e10064018
Julia MLTools <juliamltools@gmail.com>
2023-03-12 13:14:46 -0400

    page: transformer blocks (2)

--- pages/transformer-blocks/index.js
+++ pages/transformer-blocks/index.js
@@ -11,7 +11,7 @@ import { Card } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 import BasicCard from './basic-card';
-import CodeBlock from './Code';
+import CodeBlock from './code';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
