59ce0bd073c126ebf4e4a9d68580c724cb98eecb
Julia MLTools <juliamltools@gmail.com>
2023-03-13 13:29:05 -0400

    home redirect fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+}
+
 module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/transformer-blocks',
-          permanent: true,
-        },
-      ]
+    redirects: async () => {
+        return [
+            {
+                // Source Path ( from )
+                source: '/',
+
+                // Destination Path ( to )
+                destination: '/transformer-blocks',
+                permanent: true,
+            },
+        ]
     },
 }
