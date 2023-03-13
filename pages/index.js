359c11f0c5af2fd4d92cc8bb2bc7c5c5a2ab3e50
Julia MLTools <juliamltools@gmail.com>
2023-03-13 13:33:27 -0400

    home redirect fix (2)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
